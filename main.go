400b7bff0b4717826c86ee91fd36495baba42f36
John (Jack) Brown <jack@brownjohnf.com>
2017-11-17 09:09:28 -0800

    Add ability to exclude columns

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 	"strings"
 )
 
-var f = flag.String("f", "1", "field(s) to select")
+var f = flag.String("f", "", "field(s) to include")
+var F = flag.String("F", "", "field(s) to exclude")
 var s = flag.Int("s", 0, "lines to skip (headers, etc)")
 var d = flag.String("d", "", "delimiter on which to split columns (defaults to whitespace)")
 var od = flag.String("D", " ", "delimiter to use in output")
@@ -34,6 +35,8 @@ func main() {
 
 		var out []string
 		var fields []string
+		include := strings.Split(*f, ",")
+		exclude := strings.Split(*F, ",")
 
 		// Unless a delimiter was passed, split on whitespace
 		if *d != "" {
@@ -42,24 +45,51 @@ func main() {
 			fields = strings.Fields(input.Text())
 		}
 
-		// Split the columns passed in f, and loop through them
-		for _, i := range strings.Split(*f, ",") {
-			index, err := strconv.Atoi(i)
-			if err != nil {
-				fmt.Println(os.Stderr, err)
-				os.Exit(2)
+		for i, _ := range fields {
+			var skip bool
+
+			if *F != "" {
+				for _, n := range exclude {
+					num, err := strconv.Atoi(n)
+					if err != nil {
+						fmt.Println(os.Stderr, err)
+						os.Exit(2)
+					}
+
+					if i == num-1 {
+						skip = true
+						break
+					}
+				}
 			}
 
-			// Skip this row if there aren't enough columns in it
-			if len(fields) < index {
-				if *v {
-					fmt.Fprintf(os.Stderr, "%d: Insufficient fields: %v; skipping\n", line, f)
-				}
+			if skip {
 				continue
 			}
 
-			// Add the field to the output slice
-			out = append(out, fields[index-1])
+			if *f == "" {
+				out = append(out, fields[i])
+				continue
+			}
+
+			for _, n := range include {
+				num, err := strconv.Atoi(n)
+				if err != nil {
+					fmt.Println(os.Stderr, err)
+					os.Exit(2)
+				}
+
+				if len(fields) < num {
+					if *v {
+						fmt.Fprintf(os.Stderr, "%d: Insufficient fields: %v; skipping\n", line, f)
+					}
+					continue
+				}
+
+				if i == num-1 {
+					out = append(out, fields[i])
+				}
+			}
 		}
 
 		// Write-out the row's output
